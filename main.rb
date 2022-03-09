ad31485f9505853079e35de2ab9f8a6511c4ceca
Danilo <danilosantrac96@gmail.com>
2022-03-09 17:05:47 +0100

    test

--- main.rb
+++ main.rb
@@ -5,21 +5,22 @@ class Game
         self.color_pick
         puts "Please enter your name: "
         @player = gets.chomp
-        puts "Hi #{@player}, you have to choose which of the " 
+        puts "Hi #{@player}, you have to select which of these colors"
+
         @turns = 12
         self.player_plays
         
     end        
 
     def color_pick
-        @@colors = ['red', 'blue', 'green', 'yellow', 'purple']
-        @code = ['1', '1', '1', '1'].map{|color| color = @@colors[rand(5)]}
+        @@colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+        @code = ['1', '1', '1', '1'].map{|color| color = @@colors[rand(6)]}
         p @code
     end
     
     def player_plays
-        puts "#{@turns} turns remain"
-        @player_input = gets.chomp.split(' ')
+        puts "#{@turns} #{@turns == 1 ? 'turn' : 'turns'} remain"
+        @player_input = gets.chomp.downcase.split(' ')
 
         if @player_input.all?{ |str| @@colors.any?{ |color| str == color}}
             @turns -= 1
@@ -45,6 +46,7 @@ class Game
             self.reset
             puts "Thank you for playing with us #{@player}, have a nice day"
         else 
+            self.reset
             counter = {}
             @player_input.each do |color|
                 counter[color] = [] unless counter[color] 
@@ -53,22 +55,21 @@ class Game
                 color = color == @code[i] ? 2 : color
             end
             puts info_arr1
-            info_arr2 = info_arr1.map do |color|
-                # unless color.class == 'String'
-                #     color = 2
-                # end
-                # duplicate = @code.each_index.select{ |clr| clr == color}   
-                # for i in counter[color].any?{|color| color == duplicate[i]} do
+            # info_arr2 = info_arr1.map do |color|
+            #     unless color.class == 'String'
+            #         color
+            #     end
+            #     duplicate =  @code.each_index.select{ |clr| clr == color}   
+            #     for i in 0..duplicate.length-1 do
 
-                # end
-                    
-                # if 
-                #     counter[color].push(duplicate) 
-                #     color = 1
-                # else 
-                #     color = 0
-                # end
-            end       
+            #         if  counter[color].any?{|color| color == duplicate[i]}
+            #             counter[color].push(duplicate[i]) 
+            #             color = 1
+            #         else 
+            #             color = 0
+            #         end
+            #     end
+            # end       
             p info_arr2.sort
             
             self.player_plays
@@ -76,8 +77,8 @@ class Game
     end  
     
     def reset
-        answer = gets.chomp 
-        if answer == "Y" || answer == "y"
+        answer = gets.chomp.downcase 
+        if answer == "y" || answer == ""
             self.initialize
         elsif answer == "n" 
             return
